04174348365cd5d2c97bab8d4654b24a1fdfe5b5
Gabrymr <55768041+Gabrymr@users.noreply.github.com>
2019-11-27 10:00:36 +0100

    Creamos Citas.h
    Añadimos la estructura de la clase Citas.h

---
+++ Codificacion/citas.h
@@ -0,0 +1,27 @@
+#ifndef CITAS_H
+#define CITAS_H
+
+#include <string>
+
+class citas
+	{
+	
+	private:
+		string hora;
+		string consulta;
+		string medico;
+		int fecha;
+		
+	public:
+		paciente();
+		
+		string Mostrar_CitaDia(string hora, string fecha, string nombre, string apellidos)
+		string Mostrar_CitaPaciente(string hora, string fecha, string nombre, string apellidos)
+
+		void Añadir_Citas(string hora, string fecha, string consulta, string nombre, string apellidos)
+		void Modificar_Citas(string hora, string fecha, string consulta, string nombre, string apellidos)
+		void Eliminar_Cita(string hora, string fecha, string consulta, string nombre, string apellidos)
+
+	};
+
+#endif
